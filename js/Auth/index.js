83eeb021bd2f392cbfe336b80d8de7f4bc481529
Zack agba <zackagba7@gmail.com>
2021-04-23 19:50:37 +0100

    Nice commit

---
+++ js/Auth/index.js
@@ -0,0 +1,70 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in yet
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login() {
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout() {
+  firebase.auth().signOut();
+}
+
+function sendEmailVerification() {
+  firebase.auth().currentUser.sendEmailVerification().then(function () {
+    // Email Verification sent!
+    alert('Email Verification Sent!');
+  });
+}
+
+function sendPasswordReset() {
+  var email = document.getElementById('email').value;
+  firebase.auth().sendPasswordResetEmail(email).then(function () {
+    // Password Reset Email Sent!
+    alert('Password Reset Email Sent!');
+  }).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/invalid-email') {
+      alert(errorMessage);
+    } else if (errorCode == 'auth/user-not-found') {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
